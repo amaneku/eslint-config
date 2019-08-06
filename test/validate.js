bb3dc5a5479bb331b45e7abd548d1055fdb19add
mkwtys <mkwtys@gmail.com>
2019-08-06 10:43:40 +0900

    update eslint v6

--- test/validate.js
+++ test/validate.js
@@ -1,43 +1,52 @@
-const assert = require('assert');
-const Validator = require('eslint/lib/config/config-validator');
-const Environments = require('eslint/lib/config/environments');
-const Plugins = require('eslint/lib/config/plugins');
-const Rules = require('eslint/lib/rules');
+const { Linter } = require('eslint');
+const Validator = require('eslint/lib/shared/config-validator');
 
-const environments = new Environments();
-const allRules = new Rules();
-const removedRules = Object.keys(require('eslint/conf/replacements.json').rules);
-const plugins = new Plugins(environments, () => {});
+const typescriptEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const eslintPluginFlowtype = require('eslint-plugin-flowtype');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
 
-plugins.loadAll(['eslint-comments', 'prettier', 'flowtype', '@typescript-eslint']);
+const coreRules = new Linter().getRules();
+const allRules = new Map([
+  ...coreRules,
+  ...Object.entries(typescriptEslintPlugin.configs.all.rules),
+  ...Object.entries(eslintPluginFlowtype.configs.recommended.rules),
+  ...Object.entries(eslintPluginPrettier.configs.recommended.rules)
+]);
 
-function validate(name) {
-  describe(`${name}`, () => {
+const deprecatedRuleNames = new Set(
+  Array.from(allRules)
+    .filter(([, rule]) => rule && rule.meta && rule.meta.deprecated)
+    .map(([ruleId]) => ruleId)
+);
+const removedRuleNames = new Set(Object.keys(require('eslint/conf/replacements.json').rules));
+
+describe('should be a valid config.', () => {
+  function validate(name) {
     const config = require(`../lib/${name}`);
-    const rules = config.rules;
 
-    it(`valid rule options.`, () => {
-      for (const rule of Object.keys(rules)) {
-        Validator.validateRuleOptions(allRules.get(rule), rule, rules[rule], `${name}.js`);
+    it(name, () => {
+      Validator.validate(config, name, ruleId => allRules.get(ruleId));
+
+      for (const ruleId of [].concat(
+        Object.keys(config.rules || {}),
+        ...(config.overrides || []).map(c => Object.keys(c.rules || {}))
+      )) {
+        const rule = allRules.get(ruleId);
+        if (!rule) {
+          throw new Error(`The '${ruleId}' rule does not exist.`);
+        }
+        if (deprecatedRuleNames.has(ruleId)) {
+          throw new Error(`The '${ruleId}' rule was deprecated.`);
+        }
+        if (removedRuleNames.has(ruleId)) {
+          throw new Error(`The '${ruleId}' rule was removed.`);
+        }
       }
     });
+  }
 
-    it('should not include removed rules', () => {
-      for (const rule of removedRules) {
-        assert(rule in rules === false, `'${rule}' is removed rule.`);
-      }
-    });
-
-    it('should not include deprecated rules', () => {
-      for (const rule of Object.keys(rules)) {
-        const def = allRules.get(rule);
-        assert(Boolean(def && def.meta && def.meta.deprecated) === false, `'${rule}' is deprecated rule.`);
-      }
-    });
-  });
-}
-
-validate('base');
-validate('esnext');
-validate('flowtype');
-validate('typescript');
+  validate('base');
+  validate('esnext');
+  validate('flowtype');
+  validate('typescript');
+});
